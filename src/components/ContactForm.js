e7cdef89140832ce9e56c5d3ee4f40248a6c0624
Ippei Nishiyama <nishiyama@geekfeed.co.jp>
2021-09-24 10:26:29 +0900

    バリデーションを追加

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,10 +8,10 @@ const SOURCE_PHONE_NUMBER = process.env.REACT_APP_SOURCE_PHONE_NUMBER;
 
 export default function ContactForm() {
   const [credentials, setCredentials] = useState(null);
-  const [name, setName] = useState(null);
-  const [phoneNumber, setPhoneNumber] = useState(null);
-  const [address, setAddress] = useState(null);
-  const [product, setProduct] = useState(null);
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [product, setProduct] = useState("");
 
   const init = async() => {
     const crd = await Auth.currentCredentials();
@@ -40,6 +40,17 @@ export default function ContactForm() {
     console.log(phoneNumber);
     console.log(address);
     console.log(product);
+    if (name.length == 0 || phoneNumber.length == 0 || address.length == 0 || product.length == 0) {
+      alert("すべての項目を入力してください");
+      return false;
+    }
+    let eNumber = phoneNumber.replace(/[━.*‐.*―.*－.*\-.*ー.*\-]/gi,'');
+    if (!eNumber.match(/^(0[5-9]0[0-9]{8}|0[1-9][1-9][0-9]{7})$/)) {
+        alert("正しい電話番号を入力してください");
+        return false;
+    }
+    eNumber = "+81" + eNumber.substr(1);
+    console.log(eNumber);
     const connect = new AWS.Connect({
       credentials: credentials,
       apiVersion: '2017-08-08',
@@ -49,7 +60,7 @@ export default function ContactForm() {
     const params = {
       InstanceId: INSTANCE_ID,
       ContactFlowId: CONTACT_FLOW_ID,
-      DestinationPhoneNumber: phoneNumber,
+      DestinationPhoneNumber: eNumber,
       SourcePhoneNumber: SOURCE_PHONE_NUMBER,
       Attributes: {
         name: name,
@@ -60,8 +71,10 @@ export default function ContactForm() {
     connect.startOutboundVoiceContact(params,(err, data) => {
       if (err) {
         console.log(err);
+        alert(`発信に失敗しました`);
       } else {
         console.log(data);
+        alert(`発信しました!`);
       }
     });
   };
@@ -88,7 +101,7 @@ export default function ContactForm() {
       <div className="field ">
         <label className="label">電話番号</label>
         <div className="control">
-          <input className="input" type="text" placeholder="090-1111-2222" onChange={handlePhoneNumberChange} />
+          <input className="input" type="text" placeholder="+819011112222" onChange={handlePhoneNumberChange} />
         </div>
       </div>
 
